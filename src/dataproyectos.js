e2134d8bd535a4b2ccd59695dbdad75490b0430e
Axel Moncada Desktop <axelemoncada@gmail.com>
2024-05-05 23:18:26 -0500

    add projects

--- src/dataproyectos.js
+++ src/dataproyectos.js
@@ -1,17 +1,67 @@
+import papanoel from './assets/papanoel.gif';
+import imgvideo1 from './assets/videoestatico1.png'
+import imgvideo2 from './assets/videoestatico2.png'
+import tesorosOcultos from './assets/tesorosOcultos.gif'
+import logoreact from "./logos/React-b.svg"
+import logoangular from "./logos/Angular-b.svg"
+import BOOTSTRAP from "./logos/Bootstrap-b.svg"
+import logonode from "./logos/Node-b.svg"
+import logotailwind from "./logos/Tailwind-b.svg"
+import HTML from "./logos/HTML5-B.svg"
+import CSS from "./logos/css-b.svg"
+import JS from "./logos/JS-B.svg"
+import FIGMA from "./logos/FIGMA-B-2.svg"
+import ANGULAR from "./logos/Angular-b.svg"
+import PHP from "./logos/PHP-B.svg"
+import MYSQL from "./logos/MYSQLSVG-b.svg"
+
 const dataproyectos = [
     {
         id: 1,
-        company: "Empresa 1",
-        title: "Título del Proyecto 1",
-        description: "Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, "
+        company: "Banco Unión - Western Union / 2022",
+        title: "Giro Aguinaldo Millonario",
+        description: "En el año 2022, realice la campaña 'El Giro Aguinaldo Millonario', una iniciativa innovadora que fusionaba la acción de enviar dinero al exterior a través de Western Union con la emoción de participar en un juego interactivo similar a Flappy Bird. La mecánica era sencilla: cada persona que realizara un envío de dinero tenía la oportunidad de jugar al juego con cinco vidas. Aquel participante que lograra la puntuación más alta al final del periodo de la campaña, se hacía merecedor de un premio en efectivo superior a los 500 mil pesos colombianos. Fue una campaña exitosa se lograron mas de 8mil participantes y 200 ganadores generando un gran impacto a la empresa",
+        tecnologias: {
+            HTML,
+            CSS,
+            JS,
+            BOOTSTRAP,
+            FIGMA
+        },
+
+        video: papanoel,
+        img: imgvideo1
     },
     {
         id: 2,
-        company: "Empresa 2",
-        title: "Título del Proyecto 2",
-        description: "Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, "
+        company: "Banco Union / 2023",
+        title: "Tesoros escondidos - Calendario 2024",
+        description: "Trabajé con el equipo de diseño gráfico en el calendario 2024 de Banco Unión, con el objetivo de crear un calendario único y útil para los clientes. Surgió el proyecto 'Tesoros Ocultos de Colombia', un calendario que destaca 12 destinos turísticos poco conocidos en el país, cada uno con su historia, cómo llegar y consejos. Cada mes presenta un destino y tiene un QR que dirige a más información en una landing que desarrollé. El proyecto superó las expectativas al imprimir y distribuir más de 30 mil copias en todo el país, rompiendo con la idea de los calendarios tradicionales. ",
+        video: tesorosOcultos,
+        img: imgvideo2,
+        tecnologias: {
+            HTML,
+            CSS,
+            JS,
+            ANGULAR,
+            FIGMA
+        },
     },
-    // Agrega más proyectos según sea necesario
+    {
+        id: 3,
+        company: "América de cali / 2021",
+        title: "Sistema de ingreso al Pascual guerrero",
+        description: "El equipo colombiano de futbol América de cali poseía un gran problema y es que todos los empleados podían entrar gratis al estadio, generando un sin fin de problemas, para solucionar dicho inconveniente desarrolle un sistema de carga y visualización de los trabajadores actuales de la plantilla, con el fin de que gestión humana para cada partido subiera un Excel con toda la lista de empleados autorizados para ingresar y el día del partido las personas de seguridad ingresaban a la página y verificaban si podían acceder al estadio o no. ",
+        video: tesorosOcultos,
+        img: imgvideo2,
+        tecnologias: {
+            HTML,
+            CSS,
+            JS,
+            PHP,
+            MYSQL
+        },
+    },
 ];
 
 export default dataproyectos;
--- src/proyectos.jsx
+++ src/proyectos.jsx
@@ -1,35 +1,89 @@
 import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/card";
 import dataproyectos from "./dataproyectos";
+import React, { useState } from 'react';
+import { Tooltip } from "@nextui-org/tooltip";
+import csslogo from "./logos/css-b.svg"
+import logobootstrap from "./logos/Bootstrap-b.svg"
 
 function Proyectos() {
+    const [hoveredIndex, setHoveredIndex] = useState(null);
+    const tamano = 25
+
     return (
-        <div className="container mx-auto px-4 ">
+        <div className="container mx-auto w-7/12 ">
             <div className="" style={{ alignContent: 'stretch' }}>
 
 
-            {dataproyectos.map((proyecto) => (
-                    <div key={proyecto.id} className="grid grid-cols-2 gap-8 p-10">
-                        <div className="col-span-1 py-8 px-8">
-                            <h3 className="text-lg">{proyecto.company}</h3>
-                            <h2 className="text-4xl font-bold mb-4">{proyecto.title}</h2>
-                            <p>{proyecto.description}</p>
-                            <div className="tecnologias">
-                                {/* Aquí puedes agregar cualquier contenido adicional */}
-                            </div>
-                        </div>
-                        <div className="col-span-1">
-                            <Card className="py-4 px-10 bg-white/10 text-zinc-1000 h-full self-center  hover:-translate-y-1 hover:scale-102  duration-600">
-                                <CardHeader className=" flex-col items-start mb-3">
-                                    <p className="text-4xl font-bold">Herramientas</p>
-                                </CardHeader>
-                                <CardBody className="overflow-visible py-2">
-                                    {/* Contenido de las herramientas */}
-                                </CardBody>
-                            </Card>
-                        </div>
+                {dataproyectos.map((proyecto, index) => (
+                    <div key={proyecto.id} className={`grid grid-cols-6 gap-8 p-10 ${index % 2 === 0 ? '' : 'flex-row-reverse'}`}
+                        onMouseEnter={() => setHoveredIndex(index)}
+                        onMouseLeave={() => setHoveredIndex(null)}
+                    >
+                        {index % 2 === 0 ? (
+                            <>
+                                <div className="col-span-4 py-8 px-8 self-center">
+                                    <h3 className="text-lg">{proyecto.company}</h3>
+                                    <h2 className="text-4xl font-bold mb-4">{proyecto.title}</h2>
+                                    <p>{proyecto.description}</p>
+                                    <div className="flex iconwhite gap-4 mt-10">
+                                        {Object.entries(proyecto.tecnologias).map(([tecnologia, ruta]) => (
+                                            <Tooltip showArrow={true} color={"primary"} content={tecnologia}>
+                                                <img key={tecnologia} src={ruta} alt={tecnologia} width={tamano} className="tecnologia-icono" />
+                                            </Tooltip>
+                                        ))}
+
+
+                                    </div>
+                                </div>
+                                <div className="col-span-2">
+                                    <Card
+                                        style={{
+                                            backgroundImage: `url(${hoveredIndex === index ? proyecto.video : proyecto.img})`,
+                                            backgroundSize: 'cover',
+                                            backgroundPosition: 'center',
+                                            width: '100%',
+                                            height: '400px'
+                                        }}
+                                        className="py-2 px-2 bg-white/10 text-zinc-1000 h-96 self-center  hover:-translate-y-1 hover:scale-102  duration-600">
+                                    </Card>
+                                </div>
+                            </>
+                        ) : (
+                            <>
+                                <div className="col-span-2 self-center">
+                                    <Card
+                                        style={{
+                                            backgroundImage: `url(${hoveredIndex === index ? proyecto.video : proyecto.img})`,
+                                            backgroundSize: 'cover',
+                                            backgroundPosition: 'center',
+                                            width: '100%',
+                                            height: '400px'
+                                        }}
+                                        className="py-2 px-2 bg-white/10 text-zinc-1000 h-96 self-center  hover:-translate-y-1 hover:scale-102  duration-600">
+                                    </Card>
+                                </div>
+                                <div className="col-span-4 py-8 px-8">
+                                    <h3 className="text-lg">{proyecto.company}</h3>
+                                    <h2 className="text-4xl font-bold mb-4">{proyecto.title}</h2>
+                                    <p>{proyecto.description}</p>
+                                    <div className="flex iconwhite gap-4 mt-10">
+                                        {Object.entries(proyecto.tecnologias).map(([tecnologia, ruta]) => (
+                                            <Tooltip showArrow={true} color={"primary"} content={tecnologia}>
+                                                <img key={tecnologia} src={ruta} alt={tecnologia} 
+                                                    className={`
+                                            tecnologia-icono 
+                                            ${tecnologia === 'FIGMA' ? 'tecnologia-icono-grande' : ''} 
+                                            ${tecnologia === 'PHP' ? 'tecnologia-icono-small' : ''}`} />
+                                            </Tooltip>
+                                        ))}
+
+
+                                    </div>
+                                </div>
+                            </>
+                        )}
                     </div>
                 ))}
-
 
 
             </div>
